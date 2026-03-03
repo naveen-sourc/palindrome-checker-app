6cbfd1826d470ebba813bf6922ea07293d884da1
naveen-sourc <nm4108@srmist.edu.in>
2026-03-03 15:59:13 +0530

    Base application setup

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class PallindromeCheckerApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        Deque<Character> deque = new ArrayDeque<>();
+
+// Insert characters into deque
+        for (char ch : input.toCharArray()) {
+            deque.addLast(ch);
+        }
+
+        boolean isPalindrome = true;
+
+// Compare front and rear
+        while (deque.size() > 1) {
+            char front = deque.removeFirst();
+            char rear = deque.removeLast();
+
+            if (front != rear) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
+
+        sc.close();
+    }
+}
